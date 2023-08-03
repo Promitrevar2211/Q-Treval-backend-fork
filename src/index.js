9f39ecd938af731ac764b1375725ebd4775d04c5
Promit-revar <61512425+Promit-revar@users.noreply.github.com>
2023-08-03 16:12:43 +0530

    add env info on home route

--- src/index.js
+++ src/index.js
@@ -97,6 +97,7 @@ app.use("/api/notifications",notificationRoute);
 app.get("/api/home-page", homePageHandler);
 app.get("/",(req,res)=>{
   console.log(process.env.NODE_ENV);
+  res.send({env:process.env.NODE_ENV});
 })
 console.log(process.env.NODE_ENV);
 //app.get("/api/city-search",searchCityHandler);
