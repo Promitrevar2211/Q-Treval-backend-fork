1189122ee5b80b79445c4932afe9c3b4f78f597b
Promit-revar <61512425+Promit-revar@users.noreply.github.com>
2023-08-03 18:10:03 +0530

    corrected: webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default {
     entry: './server.js',
     target: 'node',
-    mode: 'development',
+    mode: 'production',
     externals: [webpackNodeExternals()],
     output: {
         filename: 'index.js',
